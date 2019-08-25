a89887c22d67195316d4b09f1882adf8a9135404
akif999 <aki01nf28@gmail.com>
2019-08-25 21:05:24 +0900

    perceptron gates

--- deep_learning_from_scratch/sandbox/sandbox.py
+++ deep_learning_from_scratch/sandbox/sandbox.py
@@ -1,5 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.array([1, 2])
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    if tmp > theta:
+        return 1
 
-print(x)
+print("AND")
+print(AND(0, 0))
+print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
+
+def NAND(x1, x2):
+    w1, w2, theta = -0.5, -0.5, -0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    if tmp > theta:
+        return 1
+
+print("NAND")
+print(NAND(0, 0))
+print(NAND(0, 1))
+print(NAND(1, 0))
+print(NAND(1, 1))
+
+def OR(x1, x2):
+    w1, w2, theta = 1.0, 1.0, 0.5
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    if tmp > theta:
+        return 1
+
+print("OR")
+print(OR(0, 0))
+print(OR(0, 1))
+print(OR(1, 0))
+print(OR(1, 1))
